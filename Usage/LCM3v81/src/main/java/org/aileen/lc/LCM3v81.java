7f7adf50c895571b51a18d537498a6db4a56ceea
Eugene-Forest <1244303915@qq.com>
2024-12-27 15:24:39 +0800

    feat: 添加 LC 项目

---
+++ Usage/LCM3v81/src/main/java/org/aileen/lc/LCM3v81.java
@@ -0,0 +1,14 @@
+package org.aileen.lc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eugene-Forest
+ * {@code @date} 2024/12/27
+ */
+@SpringBootApplication
+public class LCM3v81 {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(LCM3v81.class, args);
+    }
+}
