c6d5a526e316b0c27e24efdbea27d500865374de
Eugene-Forest <1244303915@qq.com>
2025-04-07 18:06:27 +0800

    test: 补充测试Control

--- Usage/DataSourceSetTest/src/main/java/org/aileen/datasourcesettest/controller/MssqlTestController.java
+++ Usage/DataSourceSetTest/src/main/java/org/aileen/datasourcesettest/controller/MssqlTestController.java
@@ -3,8 +3,12 @@ package org.aileen.datasourcesettest.controller;
 import org.aileen.datasourcesettest.client.MssqlTestService;
 import org.aileen.datasourcesettest.model.MssqlTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/mssql")
@@ -13,18 +17,22 @@ public class MssqlTestController {
     @Autowired
     private MssqlTestService mssqlTestService;
 
+    @GetMapping("/get")
     public MssqlTest getMssqlTest(Long id) {
         return mssqlTestService.getMssqlTest(id);
     }
 
+    @PostMapping("/insert")
     public MssqlTest insertMssqlTest(MssqlTest mssqlTest) {
         return mssqlTestService.insertMssqlTest(mssqlTest);
     }
 
+    @PostMapping("/update")
     public MssqlTest updateMssqlTest(MssqlTest mssqlTest) {
         return mssqlTestService.updateMssqlTest(mssqlTest);
     }
 
+    @GetMapping("/delete")
     public void deleteMssqlTest(Long id) {
         mssqlTestService.deleteMssqlTest(id);
     }
--- Usage/DataSourceSetTest/src/main/java/org/aileen/datasourcesettest/controller/MysqlTestController.java
+++ Usage/DataSourceSetTest/src/main/java/org/aileen/datasourcesettest/controller/MysqlTestController.java
@@ -3,6 +3,8 @@ package org.aileen.datasourcesettest.controller;
 import org.aileen.datasourcesettest.client.MysqlTestService;
 import org.aileen.datasourcesettest.model.MysqlTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,18 +15,22 @@ public class MysqlTestController {
     @Autowired
     private MysqlTestService mysqlTestService;
 
+    @GetMapping("/get")
     public MysqlTest getMysqlTest(Long id){
         return mysqlTestService.getMysqlTest(id);
     }
 
+    @PostMapping("/insert")
     public MysqlTest insertMysqlTest(MysqlTest mysqlTest){
         return mysqlTestService.insertMysqlTest(mysqlTest);
     }
 
+    @PostMapping("/update")
     public MysqlTest updateMysqlTest(MysqlTest mysqlTest){
         return mysqlTestService.updateMysqlTest(mysqlTest);
     }
 
+    @GetMapping("/delete")
     public void deleteMysqlTest(Long id){
         mysqlTestService.deleteMysqlTest(id);
     }
